b29d981a759625d24d0a7966db185c1b5941f961
Sylvie Vandaele <bahia3366@gmail.com>
2012-05-05 18:14:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiometticoApp::Application.config.secret_token = '00869e1f0b7c05d110fe7fc2f5753ae1d925fc5e3b92028a7af785fbcee1572a84f09bb762c6ae4185c7fd96e5476dde12da5ee0238fa05631d743f93e634477'
